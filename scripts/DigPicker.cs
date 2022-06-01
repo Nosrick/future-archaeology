042852002f51728d398c2aefb3cdc71238a878dd
nosrick <nosrick@gmail.com>
2022-06-01 17:29:39 +0100

    First pass of click to dig Tile selection needs a LOT of work

---
+++ scripts/DigPicker.cs
@@ -0,0 +1,84 @@
+using Godot;
+using System;
+using DiggyDig.scripts;
+
+public class DigPicker : RayCast
+{
+    protected const float RayLength = 1000f;
+
+    [Export] protected NodePath CameraPath;
+    [Export] protected NodePath GridPath;
+
+    protected Camera MyCamera { get; set; }
+    protected GridMap DigMap { get; set; }
+    
+    protected Vector2 MousePosition { get; set; }
+    protected bool CastRay { get; set; }
+    
+    public override void _Ready()
+    {
+        if (this.CameraPath is null)
+        {
+            GD.Print("CAMERA PATH IS NULL");
+            return;
+        }
+
+        if (this.GridPath is null)
+        {
+            GD.Print("GRID PATH IS NULL");
+            return;
+        }
+
+        this.MyCamera = this.GetNodeOrNull<Camera>(this.CameraPath);
+        this.DigMap = this.GetNodeOrNull<GridMap>(this.GridPath);
+    }
+
+    public override void _Input(InputEvent @event)
+    {
+        base._Input(@event);
+
+        if (@event is InputEventMouseButton mouseButton
+            && mouseButton.Pressed
+            && mouseButton.ButtonIndex == (int) ButtonList.Left)
+        {
+            this.MousePosition = mouseButton.Position;
+            this.CastRay = true;
+        }
+    }
+
+    public override void _PhysicsProcess(float delta)
+    {
+        base._PhysicsProcess(delta);
+
+        if (this.CastRay)
+        {
+            var origin = this.MyCamera?.ProjectRayOrigin(this.MousePosition);
+            var direction = origin + this.MyCamera?.ProjectRayNormal(this.MousePosition) * RayLength;
+
+            if (!origin.HasValue || !direction.HasValue)
+            {
+                this.CastRay = false;
+                return;
+            }
+
+            var spaceState = this.GetWorld().DirectSpaceState;
+            this.ForceRaycastUpdate();
+            GD.Print("CLICK");
+
+            if (this.IsColliding())
+            {
+                var iRay = spaceState.IntersectRay(origin.Value, direction.Value);
+                if (iRay.Contains("position"))
+                {
+                    Vector3 point = (Vector3) iRay["position"];
+                    Vector3Int pointInt = new Vector3Int(point);
+                    GD.Print(pointInt);
+                    
+                    this.DigMap?.SetCellItem(pointInt.x, pointInt.y, pointInt.z, -1);
+                }
+            }
+
+            this.CastRay = false;
+        }
+    }
+}
---
+++ scripts/Vector3Int.cs
@@ -0,0 +1,28 @@
+﻿using Godot;
+
+namespace DiggyDig.scripts
+{
+    public struct Vector3Int
+    {
+        public int x, y, z;
+
+        public Vector3Int(int x, int y, int z)
+        {
+            this.x = x;
+            this.y = y;
+            this.z = z;
+        }
+
+        public Vector3Int(Vector3 vec)
+        {
+            this.x = (int) vec.x;
+            this.y = (int) vec.y;
+            this.z = (int) vec.z;
+        }
+
+        public override string ToString()
+        {
+            return this.x + ", " + this.y + ", " + this.z;
+        }
+    }
+}
