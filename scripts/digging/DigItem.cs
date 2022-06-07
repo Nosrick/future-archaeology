e4ad4aad00faefb3ac50137ad9f19fdd64d07f94
nosrick <nosrick@gmail.com>
2022-06-07 11:42:47 +0100

    Adding DiggingObject so we can start digging things

---
+++ scripts/digging/DigItem.cs
@@ -0,0 +1,27 @@
+﻿using Godot;
+
+namespace DiggyDig.scripts.digging
+{
+    public class DigItem : StaticBody
+    {
+        protected MeshInstance ObjectMesh;
+
+        protected CollisionShape CollisionShape;
+
+        public override void _Ready()
+        {
+            base._Ready();
+
+            this.ObjectMesh = this.GetNode<MeshInstance>("ObjectMesh");
+            this.CollisionShape = this.GetNode<CollisionShape>("CollisionShape");
+
+            this.CollisionShape.Shape = this.ObjectMesh.Mesh.CreateConvexShape();
+        }
+
+        public void AssignObject(MeshInstance meshInstance)
+        {
+            this.ObjectMesh = meshInstance;
+            this.CollisionShape.Shape = this.ObjectMesh.Mesh.CreateConvexShape();
+        }
+    }
+}
