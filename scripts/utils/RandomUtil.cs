240f1513a337a6d59b99b6637dd327d9a198a0f3
nosrick <nosrick@gmail.com>
2022-06-26 01:34:39 +0100

    Adds +/- coin flip utility

---
+++ scripts/utils/RandomUtil.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Godot;
+
+namespace ATimeGoneBy.scripts.utils
+{
+    public static class RandomUtil
+    {
+        private static readonly Random RANDOM = new Random();
+
+        private static readonly ulong START_TIME = OS.GetUnixTime();
+
+        public static int Ticks => (int) (OS.GetUnixTime() - START_TIME);
+
+        public static int PosNegCoinFlip()
+        {
+            return RANDOM.Next() % 2 == 0 ? -1 : 1;
+        }
+    }
+}
